b5d9b767eab2209c1ea69a52523955530c525578
Shangwe Nyota <s.nyota@wustl.edu>
2024-11-12 22:21:12 -0600

    Refactored app.jsx to include a square component filled with 1s

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 //Export keyword makes the function accessible outside the file
+
+function Square() {
+  return <button className="square">1</button>;
+}
+
 export default function Board() {
   return (
-  <>
-  <div className="board-row">
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-  </div>
-  <div className="board-row">
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-  </div>  <div className="board-row">
-    <button className="square">X</button>
-    <button className="square">X</button>
-    <button className="square">X</button>
-  </div>
-  </>
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
   );
 }
