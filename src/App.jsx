9abe246bd87149038ba77154475919cdad52dece
Shangwe Nyota <s.nyota@wustl.edu>
2024-11-13 00:38:32 -0600

    Added messages for winner, next move

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,21 @@ function Square({value, onSquareClick}) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true); //x starts out try by default
   const [squares, setSquares] = useState(Array(9).fill(null)); //Set board to 9 empty squares
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next Player: " + (xIsNext ? "X" : "O");
+  }
 
   function handleSquareClick(i) {
-    console.log("square clicked")
-    if (squares[i]) {
+
+    if (squares[i] || calculateWinner(squares)) {
       return; //if the Square has already been selected return early
     }
+    console.log("move starting")
+
     const nextSquares = squares.slice(); //make a copy of the data (no reference)
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -27,6 +36,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleSquareClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleSquareClick(1)}/>
@@ -44,4 +54,26 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+
+//returns null if no winner, else returns square (the winner)
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
