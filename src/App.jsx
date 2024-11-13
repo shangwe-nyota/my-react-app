053b0ddb63cf004dc117cdebd8b06e2db566de0f
Shangwe Nyota <s.nyota@wustl.edu>
2024-11-12 22:29:13 -0600

    Added useState to the Square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-//Export keyword makes the function accessible outside the file
+import { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  //value is value we store, setValue is function to change the value
+  const [value, setValue] = useState(null); //state how we remember things in react
+  function handleClick() {
+    console.log("Clicked!");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
+//Export keyword makes the function accessible outside the file
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square  value="1"/>
-        <Square  value="2"/>
-        <Square value="3"/>
+        <Square value />
+        <Square value />
+        <Square value />
       </div>
       <div className="board-row">
-        <Square value="4"/>
-        <Square value="5"/>
-        <Square value="6"/>
+        <Square value />
+        <Square value />
+        <Square value />
       </div>
       <div className="board-row">
-        <Square value="7"/>
-        <Square value="8"/>
-        <Square value="9"/>
+        <Square value />
+        <Square value />
+        <Square value />
       </div>
     </>
   );
