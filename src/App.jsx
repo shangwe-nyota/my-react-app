829623ef96bc006d5ad00ad2fcb8443353d191f5
Shangwe Nyota <s.nyota@wustl.edu>
2024-11-12 23:49:38 -0600

    Added a useState hook for squares into the board component

--- src/App.jsx
+++ src/App.jsx
@@ -17,22 +17,23 @@ function Square() {
 
 //Export keyword makes the function accessible outside the file
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null)); //Set board to 9 empty squares
   return (
     <>
       <div className="board-row">
-        <Square value />
-        <Square value />
-        <Square value />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square value />
-        <Square value />
-        <Square value />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square value />
-        <Square value />
-        <Square value />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
